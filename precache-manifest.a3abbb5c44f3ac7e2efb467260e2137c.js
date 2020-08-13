bc1b9413e55c392482f95b9dc8f43754be659707
dmytrozaremba <dmytrozaremba@gmail.com>
2020-08-13 14:29:40 +0300

    Updates

---
+++ precache-manifest.a3abbb5c44f3ac7e2efb467260e2137c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "738dba88c68e71bb07529bcb57bb044a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a28d9f9ebb4c14b7f97",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c07666123179d4166173",
+    "url": "/static/css/main.c464b59e.chunk.css"
+  },
+  {
+    "revision": "2a28d9f9ebb4c14b7f97",
+    "url": "/static/js/2.0d9f6e07.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.0d9f6e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07666123179d4166173",
+    "url": "/static/js/main.30650d2a.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facerecognitionbrain/precache-manifest.cebf849ebfe52801232e661aa578737f.js"
+  "/precache-manifest.a3abbb5c44f3ac7e2efb467260e2137c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facerecognitionbrain/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
