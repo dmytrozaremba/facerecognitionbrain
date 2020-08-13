48ba9edd493f31909dcbf1d7e80b18505dded66e
dmytrozaremba <dmytrozaremba@gmail.com>
2020-08-13 04:10:24 +0300

    Updates

---
+++ precache-manifest.cebf849ebfe52801232e661aa578737f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0240b86164da4a6be93242ae04f64afa",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "2a28d9f9ebb4c14b7f97",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61be3c2646c195fef520",
+    "url": "/facerecognitionbrain/static/css/main.c464b59e.chunk.css"
+  },
+  {
+    "revision": "2a28d9f9ebb4c14b7f97",
+    "url": "/facerecognitionbrain/static/js/2.0d9f6e07.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.0d9f6e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61be3c2646c195fef520",
+    "url": "/facerecognitionbrain/static/js/main.2750b447.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
